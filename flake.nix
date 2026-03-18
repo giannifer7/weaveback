535f43bbf82bcedb0eeb35aa1f03e980a8df8811
giannifer7 <gianni.ferrarotti@gmail.com>
2026-03-18 14:56:21 +0100

    chore: release v0.3.4

--- flake.nix
+++ flake.nix
@@ -6,13 +6,13 @@
   outputs = { self, nixpkgs }:
     let
       pkgs    = nixpkgs.legacyPackages.x86_64-linux;
-      version = "0.3.3";
+      version = "0.3.4";
       base    = "https://github.com/giannifer7/azadi/releases/download/v${version}";
     in {
       packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
         pname   = "azadi";
         inherit version;
-        src     = pkgs.fetchurl { url = "${base}/azadi-musl"; sha256 = "sha256-vkasEnw7E/yHUrnL/VN/e63BUZPb5M2L4zZdDuyEGBY="; };
+        src     = pkgs.fetchurl { url = "${base}/azadi-musl"; sha256 = "sha256-jQfXUHjtUmwdw5cP0jUHIO3RVLMbbsGOE0i4L5opkCU="; };
         dontUnpack   = true;
         installPhase = "install -Dm755 $src $out/bin/azadi";
       };

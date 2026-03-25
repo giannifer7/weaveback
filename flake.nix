93aa14ab9319d274ce54dc43efca9c2219ba77fd
giannifer7 <gianni.ferrarotti@gmail.com>
2026-03-25 23:58:57 +0100

    chore: release v0.4.0

--- flake.nix
+++ flake.nix
@@ -6,13 +6,13 @@
   outputs = { self, nixpkgs }:
     let
       pkgs    = nixpkgs.legacyPackages.x86_64-linux;
-      version = "0.3.7";
+      version = "0.4.0";
       base    = "https://github.com/giannifer7/weaveback/releases/download/v${version}";
     in {
       packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
         pname   = "weaveback";
         inherit version;
-        src     = pkgs.fetchurl { url = "${base}/weaveback-musl"; sha256 = "sha256-DWNrpx/IzcRRV2vi3TmHOQqdIyVM9lg26WiwmrZZ3aY="; };
+        src     = pkgs.fetchurl { url = "${base}/weaveback-musl"; sha256 = "sha256-8o+u2Lf57ZJjYO5DoAxOT2MBQ77IuXfoRXpUPRTr1NQ="; };
         dontUnpack   = true;
         installPhase = "install -Dm755 $src $out/bin/weaveback";
       };
